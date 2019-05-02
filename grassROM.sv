53f89165a59462c8942ec9a9ee8437c890e694a4
Tulika <tulikag2@illinois.edu>
2019-05-01 23:33:31 -0500

    added grass and modified dog

---
+++ grassROM.sv
@@ -0,0 +1,20 @@
+module  grassROM(
+									input [17:0] read_address,
+									input Clk,
+									output logic [3:0] data_Out
+);
+
+// mem has width of 4 bits and a total of 40,960 addresses (needs 16 bits to address)
+logic [3:0] mem [0:149760];
+
+initial
+begin
+	 $readmemh("grass_transparent.txt", mem);
+end
+
+
+always_ff @ (posedge Clk) begin
+	data_Out<= mem[read_address];
+end
+
+endmodule
